fix: bpb computation

src/dataset/pl_module.py
@@ -7,8 +7,8 @@ from datasets import Dataset, load_dataset
 from omegaconf import DictConfig
 from torch.utils.data import DataLoader
 from tqdm import tqdm
-from transformers import DataCollatorForLanguageModeling
 
+from src.dataset.collate import ReLLamaDataCollator
 from src.tokenizer import ReLlamaTokenizer
 from src.utils import log_if_rank_zero
 
@@ -153,9 +153,7 @@ class ReLLamaDataModule(L.LightningDataModule):
         )
 
     def train_dataloader(self) -> DataLoader:
-        data_collator = DataCollatorForLanguageModeling(
-            tokenizer=self.tokenizer, mlm=False
-        )
+        data_collator = ReLLamaDataCollator(tokenizer=self.tokenizer, mlm=False)
         return DataLoader(
             self.train_dataset,
             batch_size=self.batch_size,

src/model/pl_module.py
@@ -130,7 +130,12 @@ class ReLLamaLightningModule(L.LightningModule):
         self, batch: Dict[str, torch.Tensor], batch_idx: int
     ) -> torch.Tensor:
 
-        self.compiled_step(batch["input_ids"], batch["attention_mask"], batch["labels"])
+        self.compiled_step(
+            batch["input_ids"],
+            batch["attention_mask"],
+            batch["labels"],
+            batch["avg_char_in_token"],
+        )
 
         if (batch_idx + 1) % self.cfg.training.gradient_accumulation_steps == 0:
             optimizer = self.optimizers()
@@ -214,7 +219,13 @@ class ReLLamaLightningModule(L.LightningModule):
 
         return [optimizer], [{"scheduler": scheduler, "interval": "step"}]
 
-    def _compiled_step(self, input_ids, attention_mask, labels):
+    def _compiled_step(
+        self,
+        input_ids: torch.Tensor,
+        attention_mask: torch.Tensor,
+        labels: torch.Tensor,
+        avg_char_in_token: float,
+    ) -> None:
         outputs = self(
             input_ids=input_ids,
             attention_mask=attention_mask,
@@ -223,7 +234,13 @@ class ReLLamaLightningModule(L.LightningModule):
         # Get the loss and calculate perplexity and bits per byte (BPB)
         loss = outputs.loss
         perplexity = torch.exp(loss)
-        bpb = loss * self.bpb_term
+        # bpb = loss * self.bpb_term
+
+        # Convert loss (nats) to bits
+        loss_in_bits = loss * math.log2(math.e)
+
+        # Compute bits per byte (BPB)
+        bpb = loss_in_bits / avg_char_in_token
 
         # Add metrics logging
         self.log_dict(